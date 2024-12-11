e4907a275d1a94bfb13ed1e335a93a6e2db353a4
Ivan <ivan.leris@enti.cat>
2024-12-11 14:03:30 +0100

    menu principal

---
+++ app/src/main/java/com/example/persona_app/InitActivity.kt
@@ -0,0 +1,21 @@
+package com.example.persona_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.os.Bundle
+
+class InitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_init)
+
+        //val showImageButton: Button = findViewById(R.id.menu)
+        //val menuImage: ImageView = findViewById(R.id.menudesplegable)
+
+        //showImageButton.setOnClickListener {
+            //menuImage.visibility = View.VISIBLE
+        //}
+    }
+}
--- app/src/main/java/com/example/persona_app/MainActivity.kt
+++ app/src/main/java/com/example/persona_app/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.persona_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +12,14 @@ class MainActivity : AppCompatActivity() {
 
         //Para ver la variable de username
         //val usernameEditText = findViewById<EditText>(R.id.usernameEditText)
+
+        val loginButton: Button = findViewById(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            val intent = Intent(this, InitActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
