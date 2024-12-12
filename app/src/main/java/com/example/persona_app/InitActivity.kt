38354295e5e3ae3e9486f7428c72e411b1fe9b51
Ivan <ivan.leris@enti.cat>
2024-12-12 12:33:07 +0100

    Butons menu

--- app/src/main/java/com/example/persona_app/InitActivity.kt
+++ app/src/main/java/com/example/persona_app/InitActivity.kt
@@ -5,17 +5,31 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.os.Bundle
+import android.util.Log
 
 class InitActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_init)
 
-        //val showImageButton: Button = findViewById(R.id.menu)
-        //val menuImage: ImageView = findViewById(R.id.menudesplegable)
+        val showImageButton: Button = findViewById(R.id.menuOpen)
+        val hiddenImageButton: Button = findViewById(R.id.menuClose)
+        val hiddenImageZone: Button = findViewById(R.id.CloseZone)
+        val menuImage: ImageView = findViewById(R.id.Selector)
 
-        //showImageButton.setOnClickListener {
-            //menuImage.visibility = View.VISIBLE
-        //}
+        showImageButton.setOnClickListener {
+            menuImage.visibility = View.VISIBLE
+            hiddenImageButton.visibility = View.VISIBLE
+        }
+
+        hiddenImageButton.setOnClickListener {
+            menuImage.visibility = View.INVISIBLE
+            hiddenImageButton.visibility = View.INVISIBLE
+        }
+
+        hiddenImageZone.setOnClickListener {
+            menuImage.visibility = View.INVISIBLE
+            hiddenImageButton.visibility = View.INVISIBLE
+        }
     }
 }
